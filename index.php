41f0817322f9cdd444214721363e78efc486f3f4
crisishell <73235201+crisishell@users.noreply.github.com>
2020-10-21 09:21:22 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Cuisine</title>
+</head>
+<body>
+    
+    <?php 
+        require('header.php');
+    ?>
+
+    <div class="container">
+       
+        <div class="row">
+
+            <div class="jumbotron jumbo">
+                <h1 class="display-5">Manger c'est vivre!</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 entete">
+                <h1 class="titre">Les meilleurs recette d'ici</h1>
+                <div class="row">
+                    <div class="col-xl-4">
+                        <img class="imgBanner" src="https://images.ricardocuisine.com/services/recipes/7464.jpg" alt="">
+                    </div>
+                    <div class="col-xl-4">
+                        <img class="imgBanner" src="https://s3.amazonaws.com/medias.recettesdici.com/recettes-photos/c/churros-maison-et-sauce-au-chocolat/churros-maison-et-sauce-au-chocolat.jpg" alt="">
+                    </div>
+                    <div class="col-xl-4">
+                        <img class="imgBanner" src="https://i.pinimg.com/236x/96/b3/21/96b321fa9ce041fd7c82c095799f3cf8.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 entete2">
+                <h1 class="titre"> Nous avons votre santé a coeur</h1>
+                <p class="sante">
+                La santé occupe aujourd’hui une place centrale dans les problématiques de développement international. Cette place paraît toutefois ambiguë dans les discours et politiques publiques de développement : présentée à la fois comme fin mais aussi comme moyen du développement. Réinterroger les notions de santé et de développement dans leur historicité nous permet de mieux cerner l’importance relative de la santé dans le développement. En explorant les contextes, savoirs et politiques sur trois périodes depuis 1945, nous faisons ressortir les enjeux politiques de la santé dans les stratégies actuelles de développement. La santé apparaît alors comme un élément de plus en plus important des transformations sociales contemporaines.
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 entete">
+                <h1 class="titre">Des ingrédients de choix</h1>
+                <div class="row">
+                    <div class="col-xl-4">
+                        <img class="imgBanner" src="https://www.journalacces.ca/wp-content/uploads/sites/3/2020/01/Mieux-etre_Champignons.jpg" alt="">
+                    </div>
+                    <div class="col-xl-4">
+                        <img class="imgBanner" src="https://cdn.pratico-pratiques.com/app/uploads/sites/2/2019/04/23104327/poulet-barbecue-sur-canette-de-biere.jpg" alt="">
+                    </div>
+                    <div class="col-xl-4">
+                        <img class="imgBanner" src="https://images.ricardocuisine.com/services/recipes/1074x1074_8440-1563207159.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+
+    <script src="./script/app.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
